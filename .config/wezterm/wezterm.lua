91da12228dc5df22c214dda1a0e91dd30f1394a1
pokapon2020 <60534914+pokapon2020@users.noreply.github.com>
2022-09-12 22:18:46 +0900

    feat: add wezterm colors.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require 'wezterm';
 local act = wezterm.action
 
 local defaultProgramArgs;
+local localColorSchemeDirs = wezterm.color_scheme_dirs;
 
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
   local wslDistroName = 'Ubuntu-22.04'; 
@@ -18,6 +19,8 @@ if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
     '-c',
     '/usr/libexec/nslogin'
   };
+  --table.insert(localColorSchemeDirs, os.getenv('USERPROFILE') .. '\\.config\\wezterm\\colors');
+  localColorSchemeDirs = { os.getenv('USERPROFILE') .. '\\.config\\wezterm\\colors' };
 elseif wezterm.target_triple == 'x86_64-apple-darwin' then
   defaultProgramArgs = wezterm.default_prog;
 elseif wezterm.target_triple == 'x86_64-unknown-linux-gnu' then
@@ -30,7 +33,11 @@ return {
   default_prog = defaultProgramArgs,
   font = wezterm.font("HackGen Console NFJ"),
   font_size = 14.6,
-  color_scheme = 'Dracula',
+  color_scheme_dirs = localColorSchemeDirs,
+  --color_scheme = 'Dracula',
+  color_scheme = 'Dracula (Official)',
+	tab_bar_at_bottom = false,
+  use_fancy_tab_bar = false,
   window_background_opacity = 1.0,
   text_background_opacity = 1.0,
   use_ime = true,
